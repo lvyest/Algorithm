09137e2bd54e20810a3db52314c0c3624f76fe33
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-05 19:02:18 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int T, A, B;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << "Case #" << i+1 <<": " << A << " + "<< B << " = "<<A + B << '\n';
+    }
+
+}
