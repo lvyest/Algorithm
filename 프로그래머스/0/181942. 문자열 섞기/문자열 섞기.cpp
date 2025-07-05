ac71ed5a6354b8ea6f79a8bead527987857700a5
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-05 22:24:43 +0900

    [level 0] Title: 문자열 섞기, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -5,14 +5,12 @@ using namespace std;
 
 string solution(string str1, string str2) {    
     int n = str1.length();
-    string answer = string(2 * n, '*');
-
-    answer[0] = str1[0];
-    for(int i=1; i < n; i++){
-        answer[2*i] = str1[i];
-    }
+    string answer = string(2 * n, ' ');
+    
     for(int i=0; i < n; i++){
         answer[2*i+1] = str2[i];
+        answer[2*i] = str1[i];
     }
     return answer;
 }
+
