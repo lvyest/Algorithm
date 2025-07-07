a187562ab6ec8f2a70b2766693e0f78a3296838c
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-07 21:57:08 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 
 int solution(int slice, int n) {
+    /* 내 풀이
     int answer = 0;
     int i = 1;
     while((slice * i) / n < 1){
         i ++;
     }
     answer = i;
+    return answer;
+    */
+    
+    int answer = n / slice;
+    if (n % slice != 0) answer ++; 
     return answer;
 }
