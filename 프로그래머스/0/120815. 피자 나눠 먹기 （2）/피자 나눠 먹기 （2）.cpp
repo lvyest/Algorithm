4ae63d6bd991c4b2144e6956605d603e47882f3d
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-07 21:46:38 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -3,17 +3,14 @@
 
 using namespace std;
 
+//(6 * result)은 n의배수 : 가장 작은 result  
+
 int solution(int n) {
     int answer = 0;
-    for (int i = 1; i < 999; i++){
-        if ((6 * i) % n == 0){
-            answer = i;
-            break;
-        }
-        else if(i==998){
-            break;
-        }
-    } 
-    //(6 * result)은 n의배수 : 가장 작은 result  
+    int i = 1;
+    while((6 * i) % n != 0){
+        i++;
+    }
+    answer = i;
     return answer;
 }
