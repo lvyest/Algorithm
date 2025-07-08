33bf76469daa9d01837b328ba846109f53017451
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-09 00:00:09 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    string S; 
+    cin >> S;
+
+    cout << S.length();
+}
