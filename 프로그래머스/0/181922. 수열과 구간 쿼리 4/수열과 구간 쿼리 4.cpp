96435721ef6ddb66c93234dbe865ad70da67a8b3
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-09 22:12:34 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.40 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for(int t = 0; t < queries.size(); t++){
+        int s = queries[t][0];
+        int e = queries[t][1];
+        int k = queries[t][2];
+        for(int i = 0; i < arr.size(); i++){
+            if(s <= i && i <= e && i % k == 0) arr[i]++;
+        }
+    }
+    
+    return arr;
+}
