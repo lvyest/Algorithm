091578f2a69c68c5d60f45f6f6afa74e196e83da
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-12 23:52:42 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) break;
+
+        if (b % a == 0) {
+            cout << "factor" << '\n';
+        }
+        else if (a % b == 0) {
+            cout << "multiple" << '\n';
+        }
+        else {
+            cout << "neither" << '\n';
+        }
+    }
+
+    return 0;
+}
