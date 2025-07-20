ad3b524264dad14943cbdf14b77edd074f453fab
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-20 23:27:20 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer(2);
+    for(int i = 0; i < array.size(); i++){
+        if(array[i] > answer[0]){
+            answer = {array[i], i};
+        }
+    }
+    
+    return answer;
+}
