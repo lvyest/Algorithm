3ae04af6f49a72bf2bc08f0e5fade7bd73029c16
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2025-07-24 23:29:18 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 2.25 ms, Memory: 5.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for(int i = 0 ; i < arr.size(); i++){
+        for(int t = 0; t < arr[i]; t++){
+            answer.push_back(arr[i]);
+}
+
+}
+    return answer;
+}
