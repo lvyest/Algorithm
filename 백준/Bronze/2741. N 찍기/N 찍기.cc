e4908f4f3afdd26c8c999c0de7fca1fd281d4660
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2026-01-14 00:06:46 +0900

    [Bronze V] Title: N 찍기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <=n; i++){
+        cout << i << '\n';
+    }
+
+    return 0;
+}
