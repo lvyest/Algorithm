cd0cb3c7d3c012a2e5a7562e340c303f743686f4
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2026-01-21 04:04:49 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+bool solution(int x) {
+    string s = to_string(x);
+    int sum = 0;
+    for(char c : s) sum += c - '0';
+    
+    return (x % sum ? false : true);
+}
