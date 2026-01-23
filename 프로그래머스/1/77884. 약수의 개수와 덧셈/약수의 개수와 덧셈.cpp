faf18a41a641dd1bcab66317136d2238ad9ec6f4
KAYOUNG KIM <163396166+lvyest@users.noreply.github.com>
2026-01-23 20:26:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for(int i = left; i <= right; i++){
+        int count = 0;
+        for(int j = 1; j * j <= i; j++){
+            if(i % j == 0){
+                count ++;
+                if(j * j != i){
+                    count ++;
+                }
+            }
+        }
+        answer += (count % 2 ? (-1 * i) : i);
+    }
+    return answer;
+}
